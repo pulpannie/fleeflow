7ed40d46fed9bc3cc1babf0bf8248952569d7ae9
Ubuntu <ubuntu@ip-172-31-31-254.ap-northeast-2.compute.internal>
2019-02-26 08:26:23 +0000

    fix ecosystem.config.js, last push not included properly

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+	    DATABASEURL: "mongodb://pulpannie:EUwEI6Cm1cEHmRI0@cluster0-shard-00-00-h64sd.mongodb.net:27017,cluster0-shard-00-01-h64sd.mongodb.net:27017,cluster0-shard-00-02-h64sd.mongodb.net:27017/project?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
