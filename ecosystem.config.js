606c4c13a621cda77d19d9bbe570ac284c75b764
pulpannie <32924168+pulpannie@users.noreply.github.com>
2020-08-25 01:22:35 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,8 +11,8 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development',
-	    DATABASEURL: "mongodb://pulpannie:EUwEI6Cm1cEHmRI0@cluster0-shard-00-00-h64sd.mongodb.net:27017,cluster0-shard-00-01-h64sd.mongodb.net:27017,cluster0-shard-00-02-h64sd.mongodb.net:27017/project?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
-      PORT: '5000'
+	    DATABASEURL: "",
+      PORT: ''
     },
     env_production: {
       NODE_ENV: 'production'
@@ -22,10 +22,10 @@ module.exports = {
   deploy : {
     production : {
       user : 'node',
-      host : '212.83.163.1',
+      host : '',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
+      repo : '',
+      path : '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
